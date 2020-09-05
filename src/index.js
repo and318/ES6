141d0c9518becd80c6b8e9d5bd60b924a9aeafa7
Andrés Saavedra <hola@and318.com>
2020-09-05 00:14:52 -0400

    Webpack install and test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-//const moment = require('moment');
-//console.log (moment().format("DD MM YYYY"));
+const moment = require('moment');
+console.log(moment().format("DD MM YYYY"));
 
-//const saludo = "Hola mundo con webpack";
-//console.log(saludo); 
+const saludo = "Hola mundo con webpack";
+console.log(saludo);
 
-//const saludo2 = ()=> {
-//   alert("Saludo en alert");
-//}
-//saludo2()
-import "./check"
+const saludo2 = () => {
+    alert("Saludo en alert");
+}
+saludo2()
+
 
